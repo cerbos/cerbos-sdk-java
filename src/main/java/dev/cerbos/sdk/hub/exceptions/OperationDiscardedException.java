5a6950c11eb6d5c175ff39736c8efcb7dce08d91
Charith Ellawala <charith@cerbos.dev>
2025-06-27 12:58:00 +0100

    Update protos
    Signed-off-by: Charith Ellawala <charith@cerbos.dev>

--- src/main/java/dev/cerbos/sdk/hub/exceptions/OperationDiscardedException.java
+++ src/main/java/dev/cerbos/sdk/hub/exceptions/OperationDiscardedException.java
@@ -7,17 +7,23 @@ package dev.cerbos.sdk.hub.exceptions;
 
 import dev.cerbos.api.cloud.v1.store.Store;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 public final class OperationDiscardedException extends StoreException {
     private final long currentStoreVersion;
+    private final List<String> ignoredFiles;
 
     OperationDiscardedException(Throwable cause, Optional<Store.ErrDetailOperationDiscarded> detail) {
         super(Reason.OPERATION_DISCARDED, cause);
         this.currentStoreVersion = detail.map(Store.ErrDetailOperationDiscarded::getCurrentStoreVersion).orElse(-1L);
+        this.ignoredFiles = detail.map(d -> d.getIgnoredFilesList().stream().toList()).orElse(Collections.emptyList());
     }
 
     public long getCurrentStoreVersion() {
         return currentStoreVersion;
     }
+
+    public Iterable<String> getIgnoredFiles() { return ignoredFiles;}
 }
